38c1520f9754114122ad7dc34f1665735d18c08d
zouhuigang <952750120@qq.com>
2019-09-03 16:10:33 +0800

    修复第一次不能成功授权的缓存bug

--- src/main.go
+++ src/main.go
@@ -36,50 +36,11 @@ func init() {
 
 }
 
-func main2() {
-	// err := zstorage.MyStorage.Set("weixin_service_access_token", `{
-	// 	"realname": "邹慧刚",
-	// 	"mobile": 18117000088,
-	// 	"sex": "F"
-	// }`, 120)
-	// if err != nil {
-	// 	fmt.Println("storage fail", err.Error())
-	// }
-
-	// dat := zstorage.MyStorage.Get("weixin_service_access_token")
-	// fmt.Printf("%s\n", dat)
-
-	//=====
-	// 24小时一个日志文件，最多存365天的日志文件。 再多了就删掉
-
-	// for {
-	// 	time.Sleep(time.Second * 3)
-	// 	logger.MyLogger.Info("asdsadasdsad ")
-	// 	logger.MyLogger.Errorf("=====================")
-	// }
-	logger.MyLogger.Info("success ")
-}
-
 func main() {
 
-	//测试access_token
-	//zcache.GetAccessToken()
+	port := zconfig.CFG.MustInt("parameter", "rpc_port", 3333)
 
-	//测试jsapi_ticket
-	//dat := zcache.GetJsapiTicket()
-	//dat := zservice.Jsapi_sign(`http://c3.ab.51tywy.com/qrcode/test/test.html`)
-
-	// port := zconfig.CFG.MustInt("parameter", "rpc_port", 3333)
-
-	// is_success, msg, service := zutils.StartServerThrift(port)
-	// if !is_success {
-	// 	fmt.Printf("%s\n", msg)protocolFactory
-	// 	os.Exit(3)
-	// } else {
-	// 	fmt.Printf("v%s启动成功[TCP4]，调用密码为:%s,端口为:%d\n", "1.0.0", "", port)
-	// }
-
-	ipser := `0.0.0.0:3333`
+	ipser := fmt.Sprintf("0.0.0.0:%d", port)
 	// 传输器,传输方式
 	transportFactory := thrift.NewTFramedTransportFactory(thrift.NewTTransportFactory())
 	//transportFactory := thrift.NewTBufferedTransportFactory(10000000)
